14cec6f480e473383af422940f22190667b2d35a
unknown <marisol91olmos@gmail.com>
2023-09-07 17:13:45 -0300

    VARIABLES

--- src/fundamentosjava1/FundamentosJava1.java
+++ src/fundamentosjava1/FundamentosJava1.java
@@ -7,7 +7,7 @@ package fundamentosjava1;
 
 /**
  *
- * @author Usuario
+ * @author Nadya
  */
 public class FundamentosJava1 {
 
@@ -15,7 +15,26 @@ public class FundamentosJava1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Crear 3 variables, una para la edad, ptra para el nombre, y para su salario
+        System.out.println("VARIABLES");
+        int edad= 32;
+        String nombre = "Nadya";
+        double salario =200;
+        
+        //Mostrar por pantalla Soy <nombre> tengo <edad>y mi sueldo es de <sueldo>
+        System.out.println("Soy " + nombre + " tengo "+ edad + " años y mi sueldo es de "+ salario);
+        
+        
+        //seguidos
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Salario: " + salario);
+        
+        System.out.println("---------------------");
+                
+        
+  
+       
     }
     
 }
